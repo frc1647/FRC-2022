2df39f6e9e19bbcfb6748256e5604c416b5186ea
Andrew Tedesco <andrewjtedesco@gmail.com>
2022-01-22 12:57:29 -0500

    Lift should definitely stop now

--- src/main/java/frc/robot/commands/BallLiftUp.java
+++ src/main/java/frc/robot/commands/BallLiftUp.java
@@ -39,6 +39,7 @@ public class BallLiftUp extends Command {
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    Robot.ballLift.stopBallLift();
   }
 
   // Called when another command which requires one or more of the same
